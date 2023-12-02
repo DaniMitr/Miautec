5e0318eeb268c733244e41f9be1248a7408c67ff
Daniel Modesto <142944953+DaniMitr@users.noreply.github.com>
2023-12-02 19:38:02 -0300

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,82 @@
+export default [
+  {
+    question: "Em média, quanto tempo vive um gato?",
+    answers: [
+      { option: "10 a 30 anos", correct: false },
+      { option: "5 a 10 anos", correct: false },
+      { option: "15 a 20 anos", correct: true },
+    ],
+  },
+  {
+    question: "Um gato precisa tomar banho com que frequência?",
+    answers: [
+      { option: "Alta (semanalmente)", correct: false },
+      { option: "Média (mensalmente)", correct: false },
+      { option: "Baixa (semestralmente)", correct: true },
+    ],
+  },
+  {
+    question: "Por que é recomendado a castração?",
+    answers: [
+      { option: "Prevenir doenças", correct: true },
+      { option: "Impedir gestação", correct: false },
+      { option: "Aumentar longevidade", correct: false },
+    ],
+  },
+  {
+    question: "Qual a principal causa de óbitos em gatos?",
+    answers: [
+      { option: "Velhice", correct: false },
+      { option: "Cancêr", correct: true },
+      { option: "Obesidade", correct: false },
+    ],
+  },
+  {
+    question: "Geralmente, quantos bigodes os gatos tem em cada lado?",
+    answers: [
+      { option: "12", correct: true },
+      { option: "10", correct: false },
+      { option: "14", correct: false },
+    ],
+  },
+  {
+    question: "Como evitar que as bolas de pelos prejudiquem os gatos?",
+    answers: [
+      { option: "Melhorando a alimentação", correct: false },
+      { option: "Escovando os dentes", correct: false },
+      { option: "Escovando os pelos", correct: true },
+    ],
+  },
+  {
+    question: "Qual a temperatura normal de um gato?",
+    answers: [
+      { option: "Entre 38°C e 39°C", correct: true },
+      { option: "Entre 37°C e 38°C", correct: false },
+      { option: "Entre 36°C e 37°C", correct: false },
+    ],
+  },
+  {
+    question: "Com que idade os gatos devem tomar as primeiras vacinas?",
+    answers: [
+      { option: "3 meses de idade", correct: false },
+      { option: "4 meses de idade", correct: true },
+      { option: "5 meses de idade", correct: false },
+    ],
+  },
+  {
+    question: "Como devem ser limpas as orelhas dos gatos?",
+    answers: [
+      { option: "Com cotonete úmido", correct: false },
+      { option: "Com água morna", correct: false },
+      { option: "Com algodão e gaze", correct: true },
+    ],
+  },
+  {
+    question: "Quais desses alimentos o gato NÃO deve consumir:",
+    answers: [
+      { option: "Maçã", correct: false },
+      { option: "Leite", correct: true },
+      { option: "Cenoura", correct: false },
+    ],
+  },
+];
